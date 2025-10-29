ac71d18bf4df6f085daff0f875c0c1515bf7328d
anirudhkoti1791 <anirudh.kvs17@gmail.com>
2025-10-29 13:26:54 +0530

    snet created

--- vnet.tf
+++ vnet.tf
@@ -6,3 +6,11 @@ resource "azurerm_virtual_network" "vnet" {
     location = azurerm_resource_group.rg.location
   
 }
+
+resource "azurerm_subnet" "snet" {
+    name                 = "mySubnet"
+    resource_group_name  = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    address_prefixes     = ["10.0.0.0/25"]
+  
+}
